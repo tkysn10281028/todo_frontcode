13cd9c7509dd379a81721bb57c04e4ea2714d246
tkysn10281028 <95330554+tkysn10281028@users.noreply.github.com>
2022-01-14 15:10:43 +0900

    axios導入　post要求を飛ばすテスト

--- frontcode/src/views/Home.vue
+++ frontcode/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <p>Input Your EmailAddress Here:</p>
+    <input type="text" v-model="emailaddress" /><br />
+    <p>Input Your Password Here:</p>
+    <input type="password" v-model="password" /><br />
+    <button v-on:click="submit">login</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+
+  data() {
+    return {
+      emailaddress: "",
+      password: "",
+    };
+  },
+  methods: {
+    submit() {
+      this.axios
+        .post("http://localhost:8082/", {
+          emailaddress: this.emailaddress,
+          password: this.password,
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
